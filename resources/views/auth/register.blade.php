a5873befa8feb76455b3165d76644a3d0035dbc4
arifhants <53720889+arifhants@users.noreply.github.com>
2019-08-09 14:23:52 +0700

    border login,form register,icon phone

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,21 +5,24 @@
     <div >
         <div class="main-page login-page ">
             <div class="widget-shadow">
-                <div class="login-top">
-                    <h4>Welcome back to Novus AdminPanel ! <br> Not a Member? <a href="signup.html">  Sign Up »</a> </h4>
+                <div class="register-top">
+                    <h4>SELAMAT DATANG DI PASLINE!</h4>
                 </div>
-                <div class="login-body">
+                <div class="register-body">
                     <form method="POST" action="{{ route('postLogin') }}">
                         @csrf
                         
                         <!-- <input type="text" class="user" value="{{ old('name') }}" name="email" placeholder="Full Name" required=""> -->
-                        <input type="text" class="email" value="{{ old('email') }}" name="email" placeholder="Email" required="">
-                        <input type="text" class="user" value="{{ old('phone_number') }}" name="email" placeholder="Phone Number">
+                        <input type="text" class="user" value="{{ old('email') }}" name="email" placeholder="Email" required="">
+                        <input type="text" class="phone" value="{{ old('phone_number') }}" name="email" placeholder="Phone Number">
                         <input type="password" name="password" class="lock" placeholder="Password">
                         <input type="password" name="password" class="lock" placeholder="Re-password">
                         <input type="submit" name="Sign In" value="Sign In">
                         <div class="forgot-grid">
                             <div class="clearfix"> </div>
+                        </div>
+                        <div class="forgot">
+                        <a href="signup.html">Sign Up</a>
                         </div>
                     </form>
                 </div>
